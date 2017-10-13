c248d8933d0a51fe12b82e6485336b72c1176720
Diana <diana.montout@gmail.com>
2017-10-13 13:47:46 +0200

    creation de la page les origines

---
+++ les origines.php
@@ -0,0 +1,74 @@
+
+<body>
+
+    <div class="container container_custom">
+        <div class="row row_custom">
+            <div class="col-xs-offset-1 col-xs-8 custom_col">
+                <h2>LES ORIGINES</h2>
+            </div>
+        </div>
+    </div>
+
+
+    <section>
+        <!--article origine et histoire-->
+        <div class="container container_custom">
+
+            <div class="row row_custom">
+                <div class="col-xs-offset-1 col-xs-8 custom_col">
+
+                    <h3>Le Reggae : Origines Et Histoire</h3>
+                    <p> Genre musical né en Jamaïque dans les années 1960, le reggae est le fruit d'un vaste métissage. La plus populaire des musiques jamaïcaines ne cesse d'évoluer et d'étendre son influence a travers le monde.
+                        <h4>Des origines métissées</h4>
+                        Le reggae apparu en Jamaïque à la fin des années 1960 est le résultat de nombreuses rencontres et de métissages. Il dérive de musiques jamaïcaines comme le Ska et le Rocksteady mais plonge ses racines dans les musiques traditionnelles caribéennes telles que le Mento et le Calypso et les musiques africaines importées en Jamaïque par les esclaves. <br>Ce style musical a aussi été influencé par la musique américaine de type Rythm'n blues, Jazz et Soul music, alors très en vogue sur l'île.
+                        <h4>Une étymologie incertaine</h4>
+                        L'étymologie du mot "reggae" est incertaine. Il dériverait de l'anglais regular people ("gens du peuple") ou de raggedy ("déguenillé"). D'autres sources indiquent qu'il vient de l'argot jamaïcain streggae ("femme facile"). <br> Ce qui est sûr, c'est que le terme de reggae a été utilisé pour la première fois dans la chanson Do the reggae écrite en 1968 par Toots, leader du groupe The Maytals. La petite vidéo ci-dessous vous permettra de découvrir cette chanson considérée comme l'acte de naissance de ce genre musical
+
+                        <!--image-->
+
+                        Moyen d'expression de la population noire et des ghettos, ce genre musical était dès son origine porteur d'un message politique et religieux : le rastafarisme. <br> Il s'agit d'un courant mystique prêchant le retour en Afrique de tous les descendants d'esclaves disséminés sur le continent américain et situant la terre promise des Noirs africains en Éthiopie. Un succès international Si le reggae a fait le tour du monde, c'est bien sûr grâce à Bob Marley qui a été et demeure son principal ambassadeur bien au-delà des frontières de la Jamaïque. <br> Même après avoir connu un succès fulgurant, à partir de 1973, Bob Marley continua à prôner la philosophie rasta dans ses chansons, affirmant : "La musique est la religion et la religion la musique. Le reggae est une communication, la communication la plus douce".
+
+                        <!--image-->
+
+                        En 1974, Eric Clapton se classa au sommet des hit-parades avec une reprise de "I Shot the Sheriff". A la même période, Keith Richards guitariste des Rolling Stones affiche ouvertement sa passion pour le reggae et les Clash, pourtant représentants de la scène punk londonienne, incorporent dans leur musique des éléments rythmiques de ce style. <br> En 1975, le succès du film The Harder They Come permit une percée de cette musique aux Etats-Unis. La bande-originale avait été composée par le chanteur Jimmy Cliff, qui tenait aussi le rôle principal du film.
+                        <h4>Une influence encore vivace</h4>
+                        Aujourd'hui, de nombreux styles musicaux s'inspirent du reggae et poursuivent son métissage à travers le monde.<br> Pour vous faire une idée de la diversité de cet univers musical, vous pouvez visiter le site http://www.reggae.fr/, un magazine musical dédié à toutes les mouvances du reggae.</p>
+
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section>
+        <!--le patois-->
+        <div class="container container_custom">
+
+            <div class="row row_custom">
+                <div class="col-xs-offset-1 col-xs-8 custom_col">
+                    <p>jriudsnjcjdn cdnjoshfoez fcndosjfn cod</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section>
+        <!--petit quizz-->
+        <div class="container container_custom">
+
+            <div class="row row_custom">
+                <div class="col-xs-offset-1 col-xs-8 custom_col">
+                    <p>jriudsnjcjdn cdnjoshfoez fcndosjfn cod</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+
+</body>
+
+
