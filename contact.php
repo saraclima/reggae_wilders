cf878ef955b5f331a97c986fa26d4fec10276e40
Rsam <rsam@MacBook-Pro-Rsam.local>
2017-10-13 13:49:34 +0200

    première version test de la page contact.

---
+++ contact.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+   <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <title>Contact</title>
+</head>
+<body>
+
+  <!-- formulaire de contact -->
+  
+  <div class="container container_custom">
+     
+     <div class="row col-xs-offset-1 col-xs-7">
+     <h2>CONTACTEZ NOUS</h2>
+     <p>Yo Man ! Communique ici avec nous si tu as des questions ou des suggestions, on te répond au plus vite.</p>
+     </div>
+     
+     <div class="row row_custom col-xs-offset-1 col-xs-7">
+        <p>rasta name</p>
+        
+        <div class="container">
+	<div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <div class="well well-sm">
+          <form class="form-horizontal" action="" method="post">
+          <fieldset>
+            <legend class="text-center">Contact us</legend>
+    
+            <!-- Name input-->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="name">Name</label>
+              <div class="col-md-9">
+                <input id="name" name="name" type="text" placeholder="Your name" class="form-control">
+              </div>
+            </div>
+    
+            <!-- Email input-->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="email">Your E-mail</label>
+              <div class="col-md-9">
+                <input id="email" name="email" type="text" placeholder="Your email" class="form-control">
+              </div>
+            </div>
+    
+            <!-- Message body -->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="message">Your message</label>
+              <div class="col-md-9">
+                <textarea class="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
+              </div>
+            </div>
+    
+            <!-- Form actions -->
+            <div class="form-group">
+              <div class="col-md-12 text-right">
+                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+              </div>
+            </div>
+          </fieldset>
+          </form>
+        </div>
+      </div>
+	</div>
+</div>
+         
+     </div>
+      
+  
+  
+      
+      
+      
+  </div>
+  
+ 
+   
+   
+   
+   
+    
+</body>
+</html>
