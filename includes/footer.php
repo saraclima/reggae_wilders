6e6853b828d0bc2c0fba9c1b10dbc7ca81b1a240
namG <tenamdrol@gmail.com>
2017-10-18 17:11:36 +0200

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,9 @@
-   <head>
-       <link rel="stylesheet" type="text/css" href="assets/css/footer2style.css"><!--to link my css file-->
-   </head>
-    <footer>
-    	<div id="footer">
-    		<div class="container-fluid container_custom">
-        		<div class="row row_custom"> 
 
-            		<div class="col-md-3 col_custom">
+    <footer>
+    	<div id="footer">    	
+            <div class="row full_row">
+            <div class="col-md-12 all_col">
+            		<div class="col-md-3 col_custom col_footer">
             			<ul class="unstyle"><!--change to make in css list style:none-->
             			 <li><a href="#">Actualité</a></li>
             				<li><a href="#">playlist</a></li>
@@ -14,7 +11,8 @@
             			</ul>
             		</div>        
 
-            		<div class="col-md-3 col_custom">            			<ul class="unstyle"><!--change to make in css list style:none-->
+            		<div class="col-md-3 col_custom">            			
+                        <ul class="unstyle"><!--change to make in css list style:none-->
             			 <li><a href="#">Contact</a></li>
             				<li><a href="#">Sitemap</a></li>
             				<li><a href="#">Partenaires</a></li>
@@ -24,14 +22,14 @@
             		<div class="col-md-2 col_custom">
                          <a href="https://facebook.com/">
                             <img src="icon/png/facebook.png" width="60px" height="60px">
-                            </a>
+                        </a>
 
                		 </div>
                 		
                     <div class="col-md-2 col_custom">
-                            <a href="https://twitter.com/">
+                         <a href="https://twitter.com/">
                             <img src="icon/png/twitter.png" width="60px" height="60px">
-                            </a>
+                         </a>
                     </div>
                             
 
@@ -40,10 +38,10 @@
                     <img src="icon/png/youtube.png" width="60px" height="70px"> </a>
                     
                     </div>
+            </div><!--end all_col-->
+        </div>
                 
-                </div><!--end main row-->
-    	            	
-            </div><!--end container-->
+                         
         </div> <!--footer end-->
     </footer>
 		
