95740f1d459ba39eae659ed6984654c7f2a50b0c
namG <tenamdrol@gmail.com>
2017-10-19 12:33:22 +0200

    footer ready,quoideneuf.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,51 +1,46 @@
-
-    <footer>
+<link rel="stylesheet" type="text/css" href="assets/css/footer2style.css">
+<footer>
     	<div id="footer">    	
             <div class="row full_row">
-            <div class="col-md-12 all_col">
+                <div class="col-md-12 all_col">
             		<div class="col-md-3 col_custom col_footer">
             			<ul class="unstyle"><!--change to make in css list style:none-->
-            			 <li><a href="#">Actualité</a></li>
-            				<li><a href="#">playlist</a></li>
-            				<li><a href="#">About</a></li>
+            			 <li><a href="\reggae_wilders/quoideneuf.php">Actualités</a></li>
+            			 <li><a href="\reggae_wilders/playlist.php">Playlist</a></li>
             			</ul>
             		</div>        
 
             		<div class="col-md-3 col_custom">            			
                         <ul class="unstyle"><!--change to make in css list style:none-->
-            			 <li><a href="#">Contact</a></li>
-            				<li><a href="#">Sitemap</a></li>
-            				<li><a href="#">Partenaires</a></li>
+            			 <li><a href="\reggae_wilders/contact.php">Contact</a></li>
+            			 <li><a href="\reggae_wilders/les_origin.php">About</a></li>
             			</ul>
             		</div> 
 
             		<div class="col-md-2 col_custom">
                          <a href="https://facebook.com/">
-                            <img src="icon/png/facebook.png" width="60px" height="60px">
-                        </a>
-
+                          <img src="icon/png/facebook.png" width="40px" height="40px">
+                         </a>
                		 </div>
                 		
                     <div class="col-md-2 col_custom">
                          <a href="https://twitter.com/">
-                            <img src="icon/png/twitter.png" width="60px" height="60px">
+                         <img src="icon/png/twitter.png" width="40px" height="40px">
                          </a>
                     </div>
-                            
-
+                    
                 	<div class="col-md-2 col_custom">
                         <a href="https://www.youtube.com/">
-                    <img src="icon/png/youtube.png" width="60px" height="70px"> </a>
-                    
+                         <img src="icon/png/youtube.png" width="40px" height="40px"> 
+                         </a>                
                     </div>
-            </div><!--end all_col-->
-        </div>
-                
+
+                </div><!--end all_col-->
+            </div><!--end row_full-->                
                          
         </div> <!--footer end-->
-    </footer>
-		
-    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+</footer>
+ <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
 
 </body>
 </html>
