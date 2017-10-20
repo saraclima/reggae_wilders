9e20c93e0750ec3a8f2fb594ea2f26c8a233002e
Sara Lima <clima.sara@gmail.com>
2017-10-20 11:41:09 +0200

    favicon2

--- index.php
+++ index.php
@@ -2,12 +2,20 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Reggae Wilders</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<title>Reggae Wilders</title>
+	<link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu" />
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
 
-    <link rel="stylesheet" type="text/css" href="assets/css/styleheader.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/home.css">
+	
+
+	<link rel="stylesheet" type="text/css" href="assets/css/footer2style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/home.css">
+
+
 </head>
 
 <body>
@@ -16,7 +24,7 @@
 			<div class="container_fluid container_nav">		
 				<div class="row row_nav">
 					<div class="col-md-2 col-xs-12 item_nav">
-						<a href="index.php"> Reggae Wilders</a>
+						<a href="index.php"><img src="assets/images/favirw.png" width="26px" height="26px" /> Reggae Wilders</a>
 					</div>
 					<div class="col-md-2 col-xs-12 item_nav">
 						<a href="quoideneuf.php"> Quoi de Neuf</a>
@@ -37,7 +45,7 @@
     <!--grand visuel -->
     <div class="container_fluide">
         <div class="row col-md-12">
-            <div class="text welcome-message">
+            <div class="text">
                 <h2>WELCOME TO OUR BLOG <br> REGGAE WILDERS</h2>
             </div>
             <img src="assets/images/contact.jpg" width="103%" height="100%" alt="">
