de16f46d7ab4fe7e73bd61e0be6bdbeb5ff8f30c
Diana <diana.montout@gmail.com>
2017-10-20 11:28:44 +0200

    supp twitter et quizz aside, police h4, traduction texte dubonson

--- includes/aside.php
+++ includes/aside.php
@@ -2,77 +2,67 @@
 
 
 <div class="col-md-3 hidden-xs">
-<!--search-->
-<div class="row row_custom search">
-<input type="text" class="form-control" placeholder="Search">
-<span class="input-group-addon">
+
+    <!--facebook-->
+    <div class="row row_custom fbk">
+
+        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FReggaefrOfficial&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+    </div>
+
+    <!--search-->
+    <div class="row row_custom search">
+        <input type="text" class="form-control" placeholder="Search">
+        <span class="input-group-addon">
 <button type="submit">
 <span class="glyphicon glyphicon-search"></span>
-</button>
-</span>
-</div>
+        </button>
+        </span>
+    </div>
 
-<!--bouton radio-->
-<div class="row row_custom radio">
-<iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FPigpenRadio%2Fpigpen-radio-selectors-presents-420-mixtape%2F" frameborder="0"></iframe>
-</div>
+    <!--bouton radio-->
+    <div class="row row_custom radio">
+        <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FPigpenRadio%2Fpigpen-radio-selectors-presents-420-mixtape%2F" frameborder="0"></iframe>
+    </div>
 
-<!--twitter-->
-<div class="row row_custom twitter">
-<a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="InfoWebMaster">Tweet</a>
-</div>
 
-<!--facebook-->
-<div class="row row_custom fbk">
+    <!--agenda-->
+    <div class="row row_custom agenda">
+        <ul class="liste1">
+            <li>20/10 - 22h30 : Pablo Moses, La scène Bastille</li>
+            <li>21/10 - 11h00 : I-tal, Afternoon Parc de Sceau</li>
+            <li>22/10 - 22h00 : Jah Cure, Gibus</li>
+            <li>23/10 - 19h00 : Lywox, </li>
+            <li>25/10 - 22h30 : Sound System</li>
+            <li>27/10 - 21h00 : Sanga Mama Africa, Péniche Add</li>
+            <li>27/10 - 23h00 : High Elements</li>
+            <li>28/10 - 15h30 : Here I Come, Salle Belve</li>
+            <li>28/10 - 23h30 : X Vibrations Urbaine</li>
+            <li>29/10 - 13h30 : Release Party, l'Amperage</li>
+            <li>29/10 - 19h00 : Dub Station #59, trabendo</li>
+            <li>30/10 - 20h30 : Lion Haffi R.o.a.r, MJC O TOTEM</li>
+            <li>30/10 - 23h30 : Dub Festival #15, La Rodia</li>
+            <li>06/11 - 23h30 : U Brown + Young Kulcha </li>
+            <li>08/11 - 23h30 : Paristown : Episode 3, Petit Bain,</li>
+            <li>09/11 - 23h30 : Reggae Dub Session, SALLE UX</li>
+            <li>09/11 - 23h30 : Wild, Woods and Fields, </li>
+            <li>09/11 - 23h30 : Chezidek + Irie Ites </li>
+            <li>09/11 - 23h30 : Skank Paradise</li>
+            <li>09/11 - 23h30 : Travelsounds + Mc Lucky</li>
+            <li>09/11 - 23h30 : Massive’s Corner #40</li>
+            <li>09/11 - 23h30 : Underground Bass Session</li>
+            <li>09/11 - 23h30 : Party Time Anniversary, Trabendo </li>
+            <li>09/11 - 23h30 : Wailing Trees</li>
+            <li>09/11 - 23h30 : Mo’Kalamity + The Wizards</li>
+            <li>09/11 - 23h30 : Dapatch </li>
+            <li>09/11 - 23h30 : High Legal Brothers</li>
+            <li>09/11 - 23h30 : DJ Akademy Sound System</li>
 
-<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FReggaefrOfficial&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-</div>
+        </ul>
 
-<!--agenda-->
-<div class="row row_custom agenda">
-<ul class="liste1">
-<li>20/10 - 22h30 : Pablo Moses, La scène Bastille</li>
-<li>21/10 - 11h00 : I-tal, Afternoon Parc de Sceau</li>
-<li>22/10 - 22h00 : Jah Cure, Gibus</li>
-<li>23/10 - 19h00 : Lywox, </li>
-<li>25/10 - 22h30 : Sound System</li>
-<li>27/10 - 21h00 : Sanga Mama Africa, Péniche Add</li>
-<li>27/10 - 23h00 : High Elements</li>
-<li>28/10 - 15h30 : Here I Come, Salle Belve</li>
-<li>28/10 - 23h30 : X Vibrations Urbaine</li>
-<li>29/10 - 13h30 : Release Party, l'Amperage</li>
-<li>29/10 - 19h00 : Dub Station #59, trabendo</li>
-<li>30/10 - 20h30 : Lion Haffi R.o.a.r, MJC O TOTEM</li>
-<li>30/10 - 23h30 : Dub Festival #15, La Rodia</li>
-<li>06/11 - 23h30 : U Brown + Young Kulcha </li>
-<li>08/11 - 23h30 : Paristown : Episode 3, Petit Bain,</li>
-<li>09/11 - 23h30 : Reggae Dub Session, SALLE UX</li>
-<li>09/11 - 23h30 : Wild, Woods and Fields, </li>
-<li>09/11 - 23h30 : Chezidek + Irie Ites </li>
-<li>09/11 - 23h30 : Skank Paradise</li>
-<li>09/11 - 23h30 : Travelsounds + Mc Lucky</li>
-<li>09/11 - 23h30 : Massive’s Corner #40</li>
-<li>09/11 - 23h30 : Underground Bass Session</li>
-<li>09/11 - 23h30 : Party Time Anniversary, Trabendo </li>
-<li>09/11 - 23h30 : Wailing Trees</li>
-<li>09/11 - 23h30 : Mo’Kalamity + The Wizards</li>
-<li>09/11 - 23h30 : Dapatch </li>
-<li>09/11 - 23h30 : High Legal Brothers</li>
-<li>09/11 - 23h30 : DJ Akademy Sound System</li>
-
-</ul>
-
-</div>
+    </div>
 
 
 
 
-<!--petit quizz-->
-<div class="row row_custom quizz">
-<h3>Petit Quizz !</h3>
-<div class="bouton">
-<button class="button">Ca roule ! </button>
-</div>
-</div>
 
 </div>
