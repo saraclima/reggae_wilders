36ddc9ceb3c6daf417825d0819cdd09534e6c4ac
Diana <diana.montout@gmail.com>
2017-10-20 13:27:36 +0200

    ajouts sur agenda

--- includes/aside.php
+++ includes/aside.php
@@ -41,21 +41,37 @@
             <li>29/10 - 19h00 : Dub Station #59, trabendo</li>
             <li>30/10 - 20h30 : Lion Haffi R.o.a.r, MJC O TOTEM</li>
             <li>30/10 - 23h30 : Dub Festival #15, La Rodia</li>
-            <li>06/11 - 23h30 : U Brown + Young Kulcha </li>
+            <li>06/11 - 23h30 : U Brown + Young Kulcha, LaBelve</li>
             <li>08/11 - 23h30 : Paristown : Episode 3, Petit Bain,</li>
             <li>09/11 - 23h30 : Reggae Dub Session, SALLE UX</li>
-            <li>09/11 - 23h30 : Wild, Woods and Fields, </li>
-            <li>09/11 - 23h30 : Chezidek + Irie Ites </li>
-            <li>09/11 - 23h30 : Skank Paradise</li>
-            <li>09/11 - 23h30 : Travelsounds + Mc Lucky</li>
-            <li>09/11 - 23h30 : Massive’s Corner #40</li>
-            <li>09/11 - 23h30 : Underground Bass Session</li>
-            <li>09/11 - 23h30 : Party Time Anniversary, Trabendo </li>
-            <li>09/11 - 23h30 : Wailing Trees</li>
-            <li>09/11 - 23h30 : Mo’Kalamity + The Wizards</li>
-            <li>09/11 - 23h30 : Dapatch </li>
-            <li>09/11 - 23h30 : High Legal Brothers</li>
-            <li>09/11 - 23h30 : DJ Akademy Sound System</li>
+            <li>10/11 - 23h30 : Wild Woods and Fields, Le Bus</li>
+            <li>10/11 - 22h00 : Chezidek + Irie Ites, Memphis</li>
+            <li>11/11 - 23h00 : Skank Paradise, Le disquaire</li>
+            <li>11/11 - 21h30 : Travelsounds + Mc Lucky, le 4 sans</li>
+            <li>17/11 - 23h30 : Massive’s Corner #40, Le Sanz</li>
+            <li>18/11 - 22h30 : The Underground, TheCave</li>
+            <li>19/11 - 21h30 : Party Time Anniversary, Trabendo </li>
+            <li>24/11 - 20h30 : Wailing Trees, Savage Palace</li>
+            <li>25/11 - 23h30 : Mo’Kalamity + The Wiz, The Street</li>
+            <li>26/11 - 20h00 : Dapatch, InnaZion Club</li>
+            <li>01/12 - 21h00 : High Legal Brothers, l'Amperage</li>
+            <li>02/12 - 23h00 : DJ Akademy Sound System,</li>
+            <li>03/12 - 21h30 : Volodia, Dub Fondation</li>
+            <li>08/12 - 22h30 : DJ Mike Love, Le Concept</li>
+            <li>09/12 - 20h00 : Toots and the Maytals,</li>
+            <li>10/12 - 19h30 : Danakil, HiyaCoffeShop</li>
+            <li>15/12 - 18h30 : Legal Shot Sound System, La ROdia</li>
+            <li>16/12 - 23h30 : Kingfisha + Yanis Odua,Le Sanz</li>
+            <li>17/12 - 23h00 : Tryo, Le quai 34</li>
+            <li>22/12 - 23h30 : Ky-Mani Marley,</li>
+            <li>23/12 - 23h30 : Jah Legacy,La Peniche Verte</li>
+            <li>24/12 - 18h30 : X-Mas Dub, NewBashment</li>
+            <li>24/12 - 23h30 : Green Dawa, Panama CLub</li>
+            <li>29/12 - 23h30 : Kalash, Scene NattyDread</li>
+            <li>30/12 - 23h30 : Aba Shanti I, OneLOvePlace</li>
+            <li>31/12 - 22h30 : New years Sound System, Circle</li>
+            <li>31/12 - 23h00 : Chronixx, Le Concept</li>
+            <li>31/12 - 23h30 : Jahneration, Savage Palace</li>
 
         </ul>
 
